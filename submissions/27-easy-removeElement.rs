b4a60bd31caf6cee89f15d92d1585e9500b6718f
rikiyaota <rikiya0421.fcb6@gmail.com>
2023-07-23 09:28:31 +0900

    improve.

--- submissions/27-easy-removeElement.rs
+++ submissions/27-easy-removeElement.rs
@@ -11,19 +11,16 @@ struct Solution {}
 // @lc code=start
 impl Solution {
     pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
-        let mut k = nums.len();
-        
-        for i in 0..nums.len() {
-            if nums[i] == val {
-                nums[i] = -1;
-                k -= 1;
+        let mut not_matched = 0;
+
+        for pointer in 0..nums.len() {
+            if nums[pointer] != val {
+                nums[not_matched] = nums[pointer];
+                not_matched += 1;
             }
         }
-
-        nums.sort();
-        nums.reverse();
         
-        k as i32
+        not_matched as i32
     }
 }
 // @lc code=end
